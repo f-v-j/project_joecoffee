c4cae56c2b875a410726339c123b2770123e1c18
Vova1 <fmv51@ukr.net>
2021-12-13 20:05:10 +0200

    Formix study

--- src/Pages/MainPage/Mainpage.js
+++ src/Pages/MainPage/Mainpage.js
@@ -5,19 +5,13 @@ import ShopBlock from "../../Components/Block/ShopBlock";
 import Header from "../../Components/Header/Header";
 import MenuBar from "../../Components/MenuBar/MenuBar";
 import Footer from "../../Components/Footer/Footer";
-import {Slider} from "../../Hooks/Slider";
-import Banner1 from "../../Images/Banner-1.png";
-import Banner2 from "../../Images/Banner-2.png";
-import Banner3 from "../../Images/Banner-3.png";
-import Banner4 from "../../Images/Banner-4.png";
-import Banner5 from "../../Images/Banner-5.png";
-import Banner6 from "../../Images/Banner-6.png";
+
+import Slider from "../../Components/Slider/Slider";
+
 
 
 function MainPage(props) {
-    useEffect(() => {
-        Slider();
-    });
+
     let shopblocks=[];
     let advertblocks=[];
     for(let i=0; i<props.dispatch.shopblocks.length; i++){
@@ -31,16 +25,7 @@ function MainPage(props) {
         <div className="App">
             <Header />
             <MenuBar />
-            <div id="slider">
-                <img src={props.dispatch.mainimage} className="main_image baseimage" alt="" />
-
-                <img src={Banner1} className="main_image slider sliderstart" alt="" />
-                <img src={Banner2} className="main_image slider" alt="" />
-                <img src={Banner3} className="main_image slider" alt="" />
-                <img src={Banner4} className="main_image slider" alt="" />
-                <img src={Banner5} className="main_image slider" alt="" />
-                <img src={Banner6} className="main_image slider" alt="" />
-            </div>
+            <Slider mainimage={props.dispatch.mainimage}/>
             <div className="content">
                 <div className="flexblock">
                     {shopblocks}
